b0a325167efdf9911a2c04123461260eb4e9249b
Decker <deckerurbano@gmail.com>
2026-01-17 18:13:20 -0300

    feat: Add StreamingMinecraft component and update documentation

--- apps/web/src/app/demo/minecraft/page.tsx
+++
@@ -1,9 +0,0 @@
-import { StreamingMinecraftDemo } from "@/registry/stream/streaming-minecraft-demo";
-
-export default function MinecraftDemoPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-background">
-      <StreamingMinecraftDemo />
-    </main>
-  );
-}
